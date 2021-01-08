5391b701041791c55230a184dd6ceed207d8b616
Vander <vstecnologia@gmail.com>
2021-01-08 10:39:58 -0400

    API cidades

--- src/main/java/com/github/vsturaro/cites/api/cities/CityRepository.java
+++ src/main/java/com/github/vsturaro/cites/api/cities/CityRepository.java
@@ -1,2 +1,6 @@
-package com.github.vsturaro.cites.api.cities;public interface CityRepository {
+package com.github.vsturaro.cites.api.cities;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CityRepository extends JpaRepository<City, Long> {
 }
